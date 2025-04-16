56bf627d5660032ad1a36268126007ac4d489a5e
i7 <bakasa.andrew@gmail.com>
2025-04-16 12:13:35 +0200

    perfecting UI 3

--- app/components/Portfolio.tsx
+++ app/components/Portfolio.tsx
@@ -20,12 +20,11 @@ const Portfolio: React.FC<PortfolioProps> = () => {
                   Smart Equipement Maintenance
                 </h4>
                 <p className="text-md font-medium leading-5 h-auto md:h-48">
-                Rail equipment maintenanance by employing AI vision tech, using drone data for scalable, efficient monitoring of critical 
-                equipment, proactively addressing potential issues for cost-effectiveness and 
-                capturing engineering operation data, predict failures and provide maintenance insights, 
-                enabling effective management of rail equipment and infrastructure for optimized 
-                operations and safety.  Schedule a demo to see how this drone-powered AI solution
-                 transforms rail equipment management.
+                Employing AI vision technology and drone data enables scalable and efficient rail equipment maintenance, 
+                proactively addressing potential issues for cost-effectiveness. This approach captures engineering 
+                operation data to predict failures and provide maintenance insights, facilitating effective management 
+                of rail equipment and infrastructure for optimized operations and safety. Schedule a demo to see how 
+                this drone-powered AI solution transforms rail equipment management
                 </p>
                 <div className="flex justify-center my-4">
                   <Link
@@ -53,15 +52,14 @@ const Portfolio: React.FC<PortfolioProps> = () => {
             <div className="bg-white transition-all ease-in-out duration-400 overflow-hidden text-gray-700 hover:scale-105 rounded-lg shadow-2xl p-3">
               <div className="m-2 text-justify text-sm">
                 <h4 className="font-semibold my-4 text-lg md:text-2xl text-center mb-4 h-12">
-                  3D Modelling & VR
+                Interactive 3D Visualization
                 </h4>
                 <p className="text-md font-medium leading-5 h-auto md:h-48">
-                  We provide platform to create visualize Planning, Engineering, and Projects as
-                  an interactive 3D Bill of Quantities (BOQ) with integrated CAD models. Incorporating
-                  costing, financing, purchasing, and supplier management, we&apos;ll in cutting edge visualized views. 
-                  Participation in the design process by all stakeholders,
-                  including non-engineers, potential investors, prototyping teams, process engineers,
-                  and manufacturing/maintenance personnel is made easy.
+                  We provide a platform to create and visualize Planning, Engineering, and Projects as an interactive 
+                  3D Bill of Quantities (BOQ) with integrated CAD models. By incorporating costing, financing, 
+                  purchasing, and supplier management, we offer cutting-edge visualized views. This platform 
+                  facilitates participation in the design process for all stakeholders, including non-engineers, 
+                  potential investors, prototyping teams, process engineers, and manufacturing/maintenance personnel
                 </p>
                 <div className="flex justify-center my-4">
                   <Link
@@ -89,16 +87,14 @@ const Portfolio: React.FC<PortfolioProps> = () => {
             <div className="bg-white transition-all ease-in-out duration-400 overflow-hidden text-gray-700 hover:scale-105 rounded-lg shadow-2xl p-3">
               <div className="m-2 text-justify text-sm">
                 <h4 className="font-semibold my-4 text-lg md:text-2xl text-center mb-4 h-12">
-                  Outsource Smart Employee 
+                Outsource Business Intelligence 
                 </h4>
                 <p className="text-md font-medium leading-5 h-auto md:h-48">
-                Outsourcing smart Employee can be delegated and help with technical, business insight for your business.
-                Harness comprehensive data mining and scraping across all traditional 
-                sources (files, Word, Excel), emails, social media, and general data 
-                sources to unlock organizational insights. Identify trends, understand root causes, 
-                analyze failure modes, reduce costs, 
-                and discover better processes. Build APIs for maintenance, planning, 
-                and operations based on these findings. SE
+                Outsourcing smart employees can delegate tasks and provide technical and business insights for your business.
+                Harness comprehensive data mining and scraping across all traditional sources (files, Word, Excel), emails, 
+                social media, and general data sources to unlock organizational insights. Identify trends, understand 
+                root causes, analyze failure modes, reduce costs, and discover better processes. Build APIs for 
+                maintenance, planning, and operations based on these findings.
                 </p>
                 <div className="flex justify-center my-4">
                   <Link
@@ -126,12 +122,13 @@ const Portfolio: React.FC<PortfolioProps> = () => {
             <div className="bg-white transition-all ease-in-out duration-400 overflow-hidden text-gray-700 hover:scale-105 rounded-lg shadow-2xl p-3">
               <div className="m-2 text-justify text-sm">
                 <h4 className="font-semibold my-4 text-lg md:text-2xl text-center mb-4 h-12">
-                  ERP with Business Simulation
+                Strategic ERP with Business Simulation
                 </h4>
                 <p className="text-md font-medium leading-5 h-auto md:h-48">
-                Our ERP  Software streamline key operations (Operation, Maintenance, Planning, Finance). 
-                Customizable, efficient workflows.  Easy online access to SOPs and Catalogue 
-                enhances knowledge sharing for diverse business needs using cloud platform
+                Our ERP software streamlines core business operations, including operations, maintenance, 
+                planning, and finance, through customizable and efficient workflows. Leveraging a 
+                cloud platform, it provides easy online access to Standard Operating Procedures 
+                (SOPs) and a comprehensive catalog, enhancing knowledge sharing across diverse business needs.
                 </p>
                 <div className="flex justify-center my-4">
                   <Link
